793bd1270d15af21b931725be563f5152a717e83
AButsai <butsai@ukr.net>
2022-05-05 21:26:08 +0300

    header

---
+++ src/js/refs/getRefs.js
@@ -0,0 +1,14 @@
+function getRefs() {
+  return {
+    // Header
+    homeLink: document.querySelectorAll('[data-home]'),
+    libraryLink: document.querySelector('[data-library]'),
+    libraryWatched: document.querySelector('[data-watched]'),
+    libraryQueue: document.querySelector('[data-queue]'),
+    form: document.querySelector('.form'),
+    header: document.querySelector('.header'),
+    buttons: document.querySelector('.library'),
+  };
+}
+
+export default getRefs;
