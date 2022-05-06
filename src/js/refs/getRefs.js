aee3c2529fec6842f457362f6a9c17449b4a950b
Leevorum <di4111ua@gmail.com>
2022-05-06 16:57:40 +0300

    spinner feature

--- src/js/refs/getRefs.js
+++ src/js/refs/getRefs.js
@@ -8,6 +8,8 @@ function getRefs() {
     form: document.querySelector('.form'),
     header: document.querySelector('.header'),
     buttons: document.querySelector('.library'),
+    //Spinner
+    spinner: document.querySelector('.load-spinner'),
   };
 }
 
