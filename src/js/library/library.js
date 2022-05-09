5decf3661f0820e8c41a94d6ff38686883957c86
Ruslan Nikiforov <87496880+Leevorum@users.noreply.github.com>
2022-05-09 13:03:10 +0300

    Revert "Path fix"

--- src/js/library/library.js
+++
@@ -1,9 +0,0 @@
-import getRefs from '../refs/getRefs';
-import { renderFilmCard } from '../card-templayte/card-templayte';
-
-function createFilmsList(films) {
-  const markup = films.map(film => renderFilmCard(film)).join('');
-  getRefs().cardslist.insertAdjacentHTML('beforeend', markup);
-}
-
-export default createFilmsList;
