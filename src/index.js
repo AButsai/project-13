479d197b0009a9410629d6dd776eed9f8e6bd08f
Leevorum <di4111ua@gmail.com>
2022-05-09 13:38:26 +0300

    Revert "Merge pull request #21 from AButsai/revert-20-path_fix"
    This reverts commit 60bb09b8f4ef3520094fe24f15d2bd97b0bce18f, reversing
    changes made to 929db507dedb45df6ff0db476cdddc52c6dfce76.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,33 @@
 import './sass/main.scss';
+import './js/service/apiMovies';
+import './js/service/search';
+import './js/header/header.js';
+import './js/load_spinner/load-spinner.js';
+import createFilmsList from './js/library/library'
+import getRefs from './js/refs/getRefs';
+import onCardFilmClick from './js/modal-templayte/modal-open';
+// Для теста:
+
+// 1)запрос на популярные фильмы
+
+// const fetchPopFilms = async () => {
+//   const response = await fetch(
+//     'https://api.themoviedb.org/3/movie/popular?api_key=81d06652aef7420f17f98b937386a748&language=en-US&page=1',
+//   );
+//   const films = await response.json();
+//   return films;
+// };
+
+// fetchPopFilms().then(response => {
+//   createFilmsList(response.results);
+//   console.log(response);
+// });
+// console.log('worked');
+
+// 2)запрос на полную информацию о фильме, по индексу
+
+// export const fetchForModal = async (currentCardIndex) =>{
+//     const response =  await fetch(`https://api.themoviedb.org/3/movie/${currentCardIndex}?api_key=81d06652aef7420f17f98b937386a748&language=en-US`);
+//     const film = await response.json();
+//     return film;
+// }
